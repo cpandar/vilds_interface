7d617940171ffb9f4eb801e83e6fc5bf903dfe9a
Chethan Pandarinath <chethan@stanford.edu>
2016-07-22 23:23:14 -0700

    added ability to just load certain variables

--- vilds_import_results.m
+++ vilds_import_results.m
@@ -1,14 +1,24 @@
-function out = vilds_import_results(infile)
+function out = vilds_import_results(infile,varargin)
+%% function out = vilds_import_results(infile,varargin)
+%%
 %% imports results from vlgp
 %%
 %% inputs: 
 %%
 %%   infile: filename with stored data
+%%
+%%   optional: which variables to load
 
 
     in_info = h5info(infile);
-    for nn = 1:numel(in_info.Datasets)
-        d = in_info.Datasets(nn).Name;
+
+    if exist('varargin','var')
+        toLoad = varargin;
+    else
+        toLoad = {in_info.Datasets.name};
+    end
+    for nn = 1:numel(toLoad)
+        d = toLoad{nn};
         tmp = h5read(infile, sprintf('/%s', d));
         out.(d) = tmp;
     end
